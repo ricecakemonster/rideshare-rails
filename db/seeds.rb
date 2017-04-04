cd81b4983bca54375255aac9b223a3b0544d65bb
Danielle Birbal <birbalds@gmail.com>
2017-04-04 14:24:42 -0700

    Added CSV data to databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+driver_csv_text = File.read(Rails.root.join('lib', 'seeds', 'drivers.csv'))
+driver_csv = CSV.parse(driver_csv_text, headers: true)
+
+driver_csv.each do |row|
+    d = Driver.new
+    d.name = row['name']
+    d.vin = row['vin']
+    d.save
+end
+
+rider_csv_text = File.read(Rails.root.join('lib', 'seeds', 'passengers.csv'))
+rider_csv = CSV.parse(rider_csv_text, headers: true)
+
+rider_csv.each do |row|
+    r = Rider.new
+    r.name = row['name']
+    r.phone_num = row['phone_num']
+    r.save
+end
+
+trip_csv_text = File.read(Rails.root.join('lib', 'seeds', 'trips.csv'))
+trip_csv = CSV.parse(trip_csv_text, headers: true)
+
+trip_csv.each do |row|
+    t = Trip.new
+    t.driver_id = row['driver_id']
+    t.rider_id = row['rider_id']
+    t.date = row['date']
+    t.rating = row['rating']
+    t.cost = rand(5.0...100.0).round(2)
+    t.save
+end
