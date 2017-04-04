ad3a1ddab4e644e193482cb0ff15620e33e8010d
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-04 15:49:14 -0700

    Added Rider's detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'riders', to: 'riders#index', as: 'riders'
+  get 'riders/:id', to: 'riders#show', as: 'rider'
 end
