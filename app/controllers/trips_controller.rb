532a3d3377b206d945504cc3527df6f1481b7cee
Danielle Birbal <birbalds@gmail.com>
2017-04-05 16:00:49 -0700

    Added trip page + edit page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,25 @@
 class TripsController < ApplicationController
+    def edit
+        @trip = Trip.find(params[:id])
+    end
+
+    def update
+        trip = Trip.find(params[:id])
+        if trip.update(trip_params)
+            redirect_to trip_path(trip)
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        id = params[:id].to_i
+        @trip = Trip.find(id)
+    end
+
+    def destroy
+        trip = Trip.find(params[:id])
+        trip.destroy
+        redirect_to trips_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     resources :drivers
     resources :riders
+    resources :trips, only: [:show, :edit, :destroy]
 
     post 'riders/:id/new_trip', to: 'riders#new_trip', as: 'new_trip'
 end
