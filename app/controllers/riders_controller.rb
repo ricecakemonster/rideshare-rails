6c788930c969a41ee59ac5a563d83ca6d01cff6c
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-04 21:20:31 -0700

    Added Edit Function

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -13,10 +13,22 @@ class RidersController < ApplicationController
 
   def create
     rider = Rider.create(rider_params)
+
     redirect_to riders_path
   end
 
+  def edit
+    @rider = Rider.find(params[:id])
+  end
 
+  def update
+    rider = Rider.find(params[:id])
+    rider.update_attributes(rider_params)
+    rider.save
+
+    redirect_to rider_path
+  end
+  
   private
 
   def rider_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'riders/new', to: 'riders#new', as: 'new_rider'
   post 'riders', to: 'riders#create'
   get 'riders/:id', to: 'riders#show', as: 'rider'
+  get 'riders/:id/edit', to: 'riders#edit', as: 'edit_rider'
+  patch 'riders/:id', to: 'riders#update'
+  delete 'riders/:id', to: 'riders#destroy'
 
 end
