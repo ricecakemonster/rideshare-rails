720959a42fc8bb3bef75ee5e68f90e2ab3a5115e
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-04 23:27:36 -0700

    Validation has fixed

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -2,6 +2,6 @@ class Rider < ApplicationRecord
     has_many :trips
     validates :name, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters." }
-    validates :phone_num, format: {with: /[0-9]\-/,
+    validates :phone_num, format: {with: /\A[0-9\-]+\z/,
     message: "only allows numbers and -." }
 end
